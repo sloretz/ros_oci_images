6e9cee82224bb471954b6ca65a4cb34491dec2b7
Shane Loretz <shane.loretz@gmail.com>
2024-02-14 21:10:27 -0800

    Playing with bash scripts, they're ok but no multiarch support
    Signed-off-by: Shane Loretz <shane.loretz@gmail.com>

--- scripts/build_image_functions.bash
+++ scripts/build_image_functions.bash
@@ -2,9 +2,11 @@ function build_single_package_image() {
     rosdistro=$1
     from=$2
     nameandtag=$3
+    manifest=$4
+    arch=$5
     package=$4
 
-    container=$(buildah from $from)
+    container=$(buildah from --manifest $manifest --arch $arch $from)
     echo $container
 
     # Install ROS 2 packages
@@ -22,8 +24,10 @@ function ros2_build_ros_core() {
     rosdistro=$1
     from=$2
     nameandtag=$3
+    manifest=$4
+    arch=$5
 
-    container=$(buildah from $from)
+    container=$(buildah from --manifest $manifest --arch $arch $from)
     echo $container
 
     # Setup timezone.
@@ -45,7 +49,8 @@ function ros2_build_ros_core() {
     buildah run $container -- rm -rf /tmp/gnupghome
 
     # Setup apt sources.list
-    buildah run $container -- bash -c 'echo "deb [ signed-by=/usr/share/keyrings/ros2-latest-archive-keyring.gpg ] http://packages.ros.org/ros2/ubuntu jammy main" > /etc/apt/sources.list.d/ros2-latest.list'
+    # TODO(sloretz) Base image version (jammy) is hardcoded here. How to eliminate it?
+    buildah run $container -- bash -c "echo \"deb [ signed-by=/usr/share/keyrings/ros2-latest-archive-keyring.gpg ] http://packages.ros.org/ros2/ubuntu jammy main\" > /etc/apt/sources.list.d/ros2-latest.list"
 
     # Set environment variables.
     buildah config --env LANG=C.UTF-8 $container
@@ -79,8 +84,10 @@ function ros2_build_ros_base() {
     rosdistro=$1
     from=$2
     nameandtag=$3
+    manifest=$4
+    arch=$5
 
-    container=$(buildah from $from)
+    container=$(buildah from --manifest $manifest --arch $arch $from)
     echo $container
 
     # Install bootstrap tools
@@ -119,30 +126,160 @@ function build_desktop() {
     rosdistro=$1
     from=$2
     nameandtag=$3
+    manifest=$4
+    arch=$5
 
-    build_single_package_image $rosdistro $from $nameandtag ros-$rosdistro-desktop
+    build_single_package_image $rosdistro $from $nameandtag $manifest $arch ros-$rosdistro-desktop
 }
 
 function build_desktop_full() {
     rosdistro=$1
     from=$2
     nameandtag=$3
+    manifest=$4
+    arch=$5
 
-    build_single_package_image $rosdistro $from $nameandtag ros-$rosdistro-desktop-full
+    build_single_package_image $rosdistro $from $nameandtag $manifest $arch ros-$rosdistro-desktop-full
 }
 
-function ros2_build_perception() {
+function build_perception() {
     rosdistro=$1
     from=$2
     nameandtag=$3
+    manifest=$4
+    arch=$5
 
-    build_single_package_image $rosdistro $from $nameandtag ros-$rosdistro-perception
+    build_single_package_image $rosdistro $from $nameandtag $manifest $arch ros-$rosdistro-perception
 }
 
 function ros2_build_simulation() {
     rosdistro=$1
     from=$2
     nameandtag=$3
+    manifest=$4
+    arch=$5
 
-    build_single_package_image $rosdistro $from $nameandtag ros-$rosdistro-simulation
+    build_single_package_image $rosdistro $from $nameandtag $manifest $arch ros-$rosdistro-simulation
+}
+
+function ros1_build_ros_core() {
+    rosdistro=$1
+    from=$2
+    nameandtag=$3
+    manifest=$4
+    arch=$5
+
+    container=$(buildah from --manifest $manifest --arch $arch $from)
+    echo $container
+
+    # Setup timezone.
+    buildah run $container -- sh -c "echo 'Etc/UTC' > /etc/timezone"
+    buildah run $container -- ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime
+    buildah run $container -- apt-get update
+    buildah run $container -- apt-get install -q -y --no-install-recommends tzdata
+
+    # Install packages needed for setting up keys.
+    buildah run $container -- apt-get install -q -y --no-install-recommends dirmngr gnupg2
+
+    # Setup keys to access ROS apt repository.
+    key='C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654'
+    buildah run $container -- mkdir /tmp/gnupghome
+    buildah run $container -- gpg --homedir=/tmp/gnupghome --batch --keyserver keyserver.ubuntu.com --recv-keys "$key"
+    buildah run $container -- mkdir -p /usr/share/keyrings
+    buildah run $container -- gpg --homedir=/tmp/gnupghome --output /usr/share/keyrings/ros1-latest-archive-keyring.gpg --batch --export "$key"
+    buildah run $container -- gpgconf --kill all
+    buildah run $container -- rm -rf /tmp/gnupghome
+
+    # Setup apt sources.list
+    # TODO(sloretz) Base image version (focal) is hardcoded here. How to eliminate it?
+    buildah run $container -- bash -c "echo \"deb [ signed-by=/usr/share/keyrings/ros1-latest-archive-keyring.gpg ] http://packages.ros.org/ros/ubuntu focal main\" > /etc/apt/sources.list.d/ros1-latest.list"
+
+    # Set environment variables.
+    buildah config --env LANG=C.UTF-8 $container
+    buildah config --env LC_ALL=C.UTF-8 $container
+    buildah config --env ROS_DISTRO=$rosdistro $container
+
+    # Install ROS 1 packages.
+    buildah run $container -- apt-get update
+    buildah run $container -- apt-get install -y --no-install-recommends ros-$rosdistro-ros-core
+
+    # Create entrypoint script
+    buildah run $container -- bash -c "echo '#!/bin/bash' > /ros_entrypoint.sh"
+    buildah run $container -- bash -c "echo 'set -e' >> /ros_entrypoint.sh"
+    buildah run $container -- bash -c "echo >> /ros_entrypoint.sh"
+    buildah run $container -- bash -c "echo '# Setup ROS environment' >> /ros_entrypoint.sh"
+    buildah run $container -- bash -c "echo >> /ros_entrypoint.sh"
+    buildah run $container -- bash -c "echo 'source "/opt/ros/$rosdistro/setup.bash" --' >> /ros_entrypoint.sh"
+    buildah run $container -- bash -c "echo 'exec \"\$@\"'>> /ros_entrypoint.sh"
+    buildah run $container -- chmod +x /ros_entrypoint.sh
+    buildah config --entrypoint '["/ros_entrypoint.sh"]' $container
+    buildah config --cmd /bin/bash $container
+
+    # Cleanup unnecessary stuff
+    buildah run $container -- rm -rf /var/lib/apt/lists/*
+
+    # Tag the image.
+    buildah commit $container $nameandtag
+}
+
+function ros1_build_ros_base() {
+    rosdistro=$1
+    from=$2
+    nameandtag=$3
+    manifest=$4
+    arch=$5
+
+    container=$(buildah from --manifest $manifest --arch $arch $from)
+    echo $container
+
+    # Install bootstrap tools
+    buildah run $container -- apt-get update
+    buildah run $container -- apt-get install --no-install-recommends -y \
+                                build-essential \
+                                python3-rosdep \
+                                python3-rosinstall \
+                                python3-vcstools
+
+    # Bootstrap rosdep
+    buildah run $container -- rosdep init
+    buildah run $container -- rosdep update --rosdistro $rosdistro
+
+    # Install ROS packages
+    buildah run $container -- apt-get install -y --no-install-recommends $manifest $arch ros-$rosdistro-ros-base
+
+    # Cleanup unnecessary stuff
+    buildah run $container -- rm -rf /var/lib/apt/lists/*
+
+    # Tag the image.
+    buildah commit $container $nameandtag
+}
+
+function ros1_build_simulators() {
+    rosdistro=$1
+    from=$2
+    nameandtag=$3
+    manifest=$4
+    arch=$5
+
+    build_single_package_image $rosdistro $from $nameandtag $manifest $arch ros-$rosdistro-simulators
+}
+
+function ros1_build_robot() {
+    rosdistro=$1
+    from=$2
+    nameandtag=$3
+    manifest=$4
+    arch=$5
+
+    build_single_package_image $rosdistro $from $nameandtag $manifest $arch ros-$rosdistro-robot
+}
+
+function ros1_build_viz() {
+    rosdistro=$1
+    from=$2
+    nameandtag=$3
+    manifest=$4
+    arch=$5
+
+    build_single_package_image $rosdistro $from $nameandtag $manifest $arch ros-$rosdistro-viz
 }
--- scripts/build_ros_images.bash
+++ scripts/build_ros_images.bash
@@ -5,59 +5,77 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "${SCRIPT_DIR}/build_image_functions.bash"
 
+architectures="${ARCHITECTURES:-amd64,arm32v7,arm64v8}"
+
+function multiarch_build() {
+    # Create a manifest for the multiarch image.
+    buildah manifest create $imagename
+
+    while IFS=',' read -ra ADDR; do
+        for arch in "${ADDR[@]}"; do
+            echo $arch
+        done
+    done <<< "$architectures"
+}
+
 function build_ros1_images() {
     rosdistro=$1
     baseimage=$2
+    imagename=$3
 
-    ros1_build_ros_core     $rosdistro   $baseimage                            ros_oci_images:$rosdistro-ros-core
-    ros1_build_ros_base     $rosdistro   ros_oci_images:$rosdistro-ros-core    ros_oci_images:$rosdistro-ros-base
-    build_desktop           $rosdistro   ros_oci_images:$rosdistro-ros-base    ros_oci_images:$rosdistro-desktop
-    ros1_build_perception   $rosdistro   ros_oci_images:$rosdistro-ros-base    ros_oci_images:$rosdistro-perception
-    ros1_build_simulators   $rosdistro   ros_oci_images:$rosdistro-ros-base    ros_oci_images:$rosdistro-simulators
-    ros1_build_viz          $rosdistro   ros_oci_images:$rosdistro-ros-base    ros_oci_images:$rosdistro-viz
-    ros1_build_robot        $rosdistro   ros_oci_images:$rosdistro-ros-base    ros_oci_images:$rosdistro-robot
-    build_desktop_full      $rosdistro   ros_oci_images:$rosdistro-desktop     ros_oci_images:$rosdistro-desktop-full
+    ros1_build_ros_core     $rosdistro   $baseimage                        $imagename:$rosdistro-ros-core
+    ros1_build_ros_base     $rosdistro   $imagename:$rosdistro-ros-core    $imagename:$rosdistro-ros-base
+    build_desktop           $rosdistro   $imagename:$rosdistro-ros-base    $imagename:$rosdistro-desktop
+    build_perception        $rosdistro   $imagename:$rosdistro-ros-base    $imagename:$rosdistro-perception
+    ros1_build_simulators   $rosdistro   $imagename:$rosdistro-ros-base    $imagename:$rosdistro-simulators
+    ros1_build_viz          $rosdistro   $imagename:$rosdistro-ros-base    $imagename:$rosdistro-viz
+    ros1_build_robot        $rosdistro   $imagename:$rosdistro-ros-base    $imagename:$rosdistro-robot
+    build_desktop_full      $rosdistro   $imagename:$rosdistro-desktop     $imagename:$rosdistro-desktop-full
 }
 
 function build_ros2_images() {
     rosdistro=$1
     baseimage=$2
+    imagename=$3
 
-    ros2_build_ros_core     $rosdistro   $baseimage                            ros_oci_images:$rosdistro-ros-core
-    ros2_build_ros_base     $rosdistro   ros_oci_images:$rosdistro-ros-core    ros_oci_images:$rosdistro-ros-base
-    build_desktop           $rosdistro   ros_oci_images:$rosdistro-ros-base    ros_oci_images:$rosdistro-desktop
-    ros2_build_perception   $rosdistro   ros_oci_images:$rosdistro-ros-base    ros_oci_images:$rosdistro-perception
-    ros2_build_simulation   $rosdistro   ros_oci_images:$rosdistro-ros-base    ros_oci_images:$rosdistro-simulation
-    build_desktop_full      $rosdistro   ros_oci_images:$rosdistro-desktop     ros_oci_images:$rosdistro-desktop-full
+    ros2_build_ros_core     $rosdistro   $baseimage                        $imagename:$rosdistro-ros-core
+    ros2_build_ros_base     $rosdistro   $imagename:$rosdistro-ros-core    $imagename:$rosdistro-ros-base
+    build_desktop           $rosdistro   $imagename:$rosdistro-ros-base    $imagename:$rosdistro-desktop
+    build_perception        $rosdistro   $imagename:$rosdistro-ros-base    $imagename:$rosdistro-perception
+    ros2_build_simulation   $rosdistro   $imagename:$rosdistro-ros-base    $imagename:$rosdistro-simulation
+    build_desktop_full      $rosdistro   $imagename:$rosdistro-desktop     $imagename:$rosdistro-desktop-full
 }
 
 function print_usage() {
-    echo "Usage: build_ros_images.bash ROS_DISTRO"
+    echo "Usage: build_ros_images.bash ROS_DISTRO IMAGE_NAME"
     echo
     echo "Supported values for ROS_DISTRO are: noetic, humble, iron, rolling"
 }
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     print_usage
     exit 1
 fi
 
+rosdistro=$1
+imagename=$2
 
-case $1 in
+case $rosdistro in
     noetic)
-        build_ros1_images noetic ubuntu:focal
+        build_ros1_images noetic ubuntu:focal $imagename
         ;;
     humble)
-        build_ros2_images humble ubuntu:jammy
+        build_ros2_images humble ubuntu:jammy $imagename
         ;;
     iron)
-        build_ros2_images iron ubuntu:jammy
+        build_ros2_images iron ubuntu:jammy $imagename
         ;;
     rolling)
-        build_ros2_images rolling ubuntu:jammy
+        build_ros2_images rolling ubuntu:jammy $imagename
         ;;
     *)
         print_usage
+        multiarch_build
         exit 1
         ;;
 esac
