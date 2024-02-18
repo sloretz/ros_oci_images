159fd0ad84a7f50fdf3046a728c3cac9c52a996a
Shane Loretz <shane.loretz@gmail.com>
2024-02-18 08:09:51 -0800

    Try pinning buildah to 1.34.0
    Signed-off-by: Shane Loretz <shane.loretz@gmail.com>

---
+++ .github/install_buildah.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu -o pipefail
+
+sudo apt-get update
+sudo apt-get install -y \
+    bats \
+    btrfs-progs \
+    git \
+    go-md2man \
+    golang \
+    libapparmor-dev \
+    libglib2.0-dev \
+    libgpgme11-dev \
+    libseccomp-dev \
+    libselinux1-dev \
+    make \
+    skopeo
+
+git clone https://github.com/containers/buildah -b v1.34.0
+cd buildah
+make runc all SECURITYTAGS="apparmor seccomp"
+sudo make install install.runc
+buildah --version
